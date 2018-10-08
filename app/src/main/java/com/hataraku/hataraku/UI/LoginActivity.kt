9e505b67be87ebe855f2cc94fd9cff62fd55581b
Yoga Cahya Romadhon <yogacahya08@gmail.com>
2018-10-08 12:44:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/hataraku/hataraku/UI/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.hataraku.hataraku.UI
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.hataraku.hataraku.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/hataraku/hataraku/UI/LupaPasswordActivity.kt
@@ -0,0 +1,15 @@
+package com.hataraku.hataraku.UI
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.hataraku.hataraku.R
+import kotlinx.android.synthetic.main.activity_lupa_password.*
+
+class LupaPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lupa_password)
+        tv_email
+    }
+}
